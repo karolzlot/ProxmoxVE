08d74416b6ede079f1c03b3527a23f171f72ba0b
Karol Zlot <11590960+karolzlot@users.noreply.github.com>
2025-08-04 18:11:52 +0200

    Update monica.sh -> use 5.0.0-beta.5 version 2

--- ct/monica.sh
+++ ct/monica.sh
@@ -40,7 +40,7 @@ function update_script() {
     mv /opt/monica/ /opt/monica-backup
     msg_ok "Backup created"
 
-    fetch_and_deploy_gh_release "monica" "monicahq/monica" "prebuild" "latest" "/opt/monica" "monica-v*.tar.bz2"
+    fetch_and_deploy_gh_release "monica" "monicahq/monica" "prebuild" "5.0.0-beta.5" "/opt/monica" "monica-v*.tar.bz2"
 
     msg_info "Configuring monica"    
     cd /opt/monica/
